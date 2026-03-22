50f93d80bdde0d7eef1be1333add14542b20ebf7
Benn Herrera <benn.herrera@pm.me>
2026-03-21 17:04:38 -0700

    windows magick and inkscape binary location - try to find them at default locations.

--- maint/svg-to-ico.sh
+++ maint/svg-to-ico.sh
@@ -15,6 +15,14 @@ if [[ -z "${SVG}" || -z "${ICO}" ]]; then
 fi
 
 OS_NAME=$(uname -s)
+
+if [[ -n "${PROGRAMFILES}" ]]; then
+    WIN_PF=${PROGRAMFILES//\\//}
+    WIN_PF=/${WIN_PF/:/}
+    MAGICK_DIR=$(cd "${WIN_PF}"; echo ImageMagick-* | sort -Vr | head -1)
+    PATH="${PATH}:${WIN_PF}/Inkscape/bin:${WIN_PF}/${MAGICK_DIR}"
+fi
+
 MAGICK=$(which magick 2> /dev/null)
 if [[ ! -x "${MAGICK}" ]]; then
     MAGICK=$(which convert 2> /dev/null)
@@ -41,22 +49,16 @@ function rm_tmp_dir() {
 trap rm_tmp_dir exit
 mkdir "${TMP_DIR}"
 
-TMP_PNG="${TMP_DIR}/favicon.svg.png"
+TMP_PNG="${TMP_DIR}/tmp.png"
 
 if [[ "${OS_NAME}" = "Darwin" ]]; then
-    qlmanage -t -s 512 -o "${TMP_DIR}/" "${SVG}" 2>/dev/null
+    qlmanage -t -s 512 -o "${TMP_PNG}" "${SVG}" 2>/dev/null
 else
-    INKSCAPE=$(which inkscape)
-    if [[ ! -x "${INKSCAPE}" ]]; then
-        if [[ "${OS_NAME}" != "Linux" ]]; then
-            INKSCAPE="C:/Program Files/Inkscape/bin/inkscape.exe"
-        fi
-    fi
-    if [[ ! -x "${INKSCAPE}" ]]; then
-        echo "inkscape not installed." 1>&2
+    if !(2>&1 which inkscape) > /dev/null; then
+        echo "svg-to-ico: Inkscape not installed or not in PATH"
         exit 1
     fi
-    "${INKSCAPE}" "${SVG}" --export-type=png --export-filename="${TMP_PNG}" -w 512 2> /dev/null
+    inkscape "${SVG}" --export-type=png --export-filename="${TMP_PNG}" -w 512 2> /dev/null
 fi
 
 "${MAGICK}" "${TMP_PNG}" -define icon:auto-resize=32,16 "${ICO}"
